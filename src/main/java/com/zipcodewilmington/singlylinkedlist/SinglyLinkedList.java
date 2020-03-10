98a6c001a30e3b9646fa0f23b9895bfd0e2eb854
cwilliams <coreywllms9@gmail.com>
2020-03-10 16:16:13 -0400

    starting set up

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,40 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import javax.xml.soap.Node;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
-}
+public class SinglyLinkedList<T> {
+    private class Node {
+
+        private T element;
+        private Node next;
+
+        public Node(T element) {
+            this.element = element;
+        }
+
+        public Node(Node next, T element) {
+            this.element = element;
+            this.next = next;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public T getElement() {
+            return element;
+        }
+
+        public void setElement(T element) {
+            this.element = element;
+        }
+    }
+
+    }
