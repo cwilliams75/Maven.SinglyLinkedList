be7149cb19a08fbcde91a2f705bcad5ca8424268
cwilliams <coreywllms9@gmail.com>
2020-03-22 16:38:57 -0400

    making progress

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -6,19 +6,9 @@ import javax.xml.soap.Node;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList<T> {
+
     private Node head;
     private int listSize = 0;
-
-    public Integer getSizeOfList() {
-        return getSizeOfList();
-    }
-
-    public void addElement(T element1) {
-    }
-
-    public T getElementByIndex(int i) {
-        return getElementByIndex();
-    }
 
     private class Node {
 
@@ -27,9 +17,10 @@ public class SinglyLinkedList<T> {
 
         public Node(T element) {
             this.element = element;
+            next = null;
         }
 
-        public Node(Node next, T element) {
+        public Node(T element, Node next) {
             this.element = element;
             this.next = next;
         }
@@ -43,22 +34,106 @@ public class SinglyLinkedList<T> {
         }
 
         public T getElement() {
+
             return element;
         }
 
         public void setElement(T element) {
             this.element = element;
         }
+
     }
 
-    private void incrementListSize(){
+    private void incrementListSize() {
         listSize++;
     }
-    private void decrementListSize(){
+
+    private void decrementListSize() {
         listSize--;
     }
-    private int getListSize(){
+
+
+    public void addElement(T element) {
+        Node newNode = new Node(element);
+
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            Node last = head;
+
+            while (last.getNext() != null) {
+                last = last.getNext();
+            }
+            last.setNext(newNode);
+        }
+
+        incrementListSize();
+    }
+
+    public boolean removeElementByIndex(int index) {
+        if (index < 0 || index > getListSize() || head == null)
+            return false;
+        if (index == 0) {
+            head = head.getNext();
+            return true;
+        }
+        Node previous = null;
+        Node current = head;
+        for (int listIndex = 1; listIndex < index; listIndex++) {
+            previous = current.getNext();
+            current = current.getNext();
+        }
+        previous.setNext(current.getNext());
+        decrementListSize();
+        return true;
+    }
+
+    public boolean containsElement(T element) {
+        if (head == null) {
+            return false;
+        }
+        Node selectedElement = head;
+        if (selectedElement.getElement().equals(element)) {
+            return true;
+        }
+        for (int i = 0; i < getListSize(); i++) {
+            selectedElement = selectedElement.getNext();
+            if (selectedElement.getElement().equals(element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int findIndexByElement(T element) {
+        if (head == null) {
+            return -1;
+        }
+        return 1;
+    }
+
+    public int getListSize() {
         return listSize;
     }
 
+    public T getElementByIndex(int index) {
+        if (index < 0 || index > getListSize())
+            return null;
+        Node selectedElement = head;
+        for (int i = 0; i < index; i++) {
+            selectedElement = selectedElement.getNext();
+        }
+        return selectedElement.getElement();
+
     }
+
+    public SinglyLinkedList<T> copySameValues(SinglyLinkedList list) {
+        SinglyLinkedList<T> copyList = new SinglyLinkedList<T>();
+        for (int i = 0; i < list.getListSize(); i++) {
+            copyList.addElement(getElementByIndex(i));
+        }
+        return copyList;
+    }
+
+
+}
